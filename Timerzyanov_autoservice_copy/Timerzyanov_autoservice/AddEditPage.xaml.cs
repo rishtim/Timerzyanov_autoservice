ace956a719d3f39522c900b3df8e29bd99a061af
rishtim <russskiy.hyusten@gmail.com>
2023-10-20 13:05:27 +0500

    lab 5 DB NOT READY!!!

--- Timerzyanov_autoservice_copy/Timerzyanov_autoservice/AddEditPage.xaml.cs
+++ Timerzyanov_autoservice_copy/Timerzyanov_autoservice/AddEditPage.xaml.cs
@@ -20,9 +20,51 @@ namespace Timerzyanov_autoservice
     /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+
+        private Service _currentService = new Service();
+        public AddEditPage(Service SelectedService)
         {
             InitializeComponent();
+
+            if(SelectedService !=null)
+                _currentService = SelectedService;
+            DataContext = _currentService;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentService.Title))
+                errors.AppendLine("Укажите название услуги");
+
+            if (_currentService.Cost == 0)
+                errors.AppendLine("Укажите стоимость услуги");
+
+            if (_currentService.Discount < 0 || _currentService.Discount > 100)
+                errors.AppendLine("Укажите корректную скидку");
+
+            if (string.IsNullOrWhiteSpace(_currentService.Duration))
+                errors.AppendLine("Укажите длительность услуги");
+
+            if(errors.Length>0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentService.ID == 0)
+                Timerzyanov_autoserviceEntities.GetContext().Service.Add(_currentService);
+            try
+            {
+                Timerzyanov_autoserviceEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
--- Timerzyanov_autoservice_copy/Timerzyanov_autoservice/ServicePage.xaml.cs
+++ Timerzyanov_autoservice_copy/Timerzyanov_autoservice/ServicePage.xaml.cs
@@ -76,10 +76,7 @@ namespace Timerzyanov_autoservice
             ServiceListView.ItemsSource = CurrentServices;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Manager.MainFrame.Navigate(new AddEditPage());
-        }
+        
 
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -99,6 +96,25 @@ namespace Timerzyanov_autoservice
         private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
         {
             UpdateServices();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                Timerzyanov_autoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = Timerzyanov_autoserviceEntities.GetContext().Service.ToList();
+            }
         }
     }
 }
